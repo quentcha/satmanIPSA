4f8d9b84dc0e4aca253276b329434ecd75a0fe2c
quentcha <quentcha05@gmail.com>
2023-11-24 14:53:15 +0100

    premiers changements

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import pygame
-# colors : (60,21,24)|black bean, (105,20,14)|Blood red, (227,231,211)|Beige, (11,57,84)|Prussian blue, (152,156,148)|Battleship gray
+pygame.init()
+# colors : (60,21,24)|black bean, (105,20,14)|Blood red, (227,231,211)|Beige, (11,57,84)|Prussian blue, (152,156,148)|Battleship gray, (0,0,0)|black
+
 #créer la fenêtre de jeu
 screen = pygame.display.set_mode((1066,600), pygame.RESIZABLE) #16:9 ratio
 width, height = pygame.display.get_surface().get_size()
+pygame.display.set_caption('SATMAN')
+asteroid = pygame.image.load("asteroid.png")
 
-screen.fill((105,20,14))
 #initialiser la boucle
 running=True
 while running:
+    screen.fill((105,20,14))
+    font = pygame.font.Font('Space Angel.ttf', int((60*width)/1066))
+    text = font.render('Satellite Manager', True, (0,0,0))
+    screen.blit(text, (((80*width)/1066,(150*height)/600),(0,0)))
     pygame.display.flip() # refresh l'écran
 
     for event in pygame.event.get():# voir tout input
@@ -15,4 +22,3 @@ while running:
             running = False
         elif event.type == pygame.VIDEORESIZE:
             width, height = pygame.display.get_surface().get_size()
-            screen.fill((105,20,14))
